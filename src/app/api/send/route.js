af5219942bdde1366bc6d03215b131b1b09b88e7
rooban33 <shajithgunasekaran@gmail.com>
2024-01-21 12:52:33 +0530

    mailerror

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_jFYWF4Uk_6cHEDwrukaV5WpHMdJPHDAT2");
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
